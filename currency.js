382e7d0c5de1fc24ed8ffcfa99acf4758d3469cb
lafsar <nihlanth@gmail.com>
2013-09-28 00:30:18 -0600

    Math fixed

--- currency.js
+++ currency.js
@@ -9,96 +9,42 @@ For instance, given 7.23, it should return:
   pennies: 3
 }
 */
-var currency = function (number) {
 
-    finalnum = Math.abs(parseFloat(number));
-    if (finalnum === NaN)
-    return "Please enter a valid number";
-    finalnum = finalnum.toString();
-    for(var i=0; i<=finalnum.length-1; i++) {
-        if(finalnum[i-3] ==='.')
-            finalnum = finalnum.slice(0,i); //truncate extraneous digits
-        
-    }
+//convert int, float or string to valid format first
+var currency = function (number) {
+var denominations = [1,5,10,25,100,500,1000,2000,5000,10000];  
+    var finalnum = number.toString();
     //console.log(finalnum);
-    return finalnum;
+    for(var i=0; i<=finalnum.length-1; i++) {
+        if(finalnum[i] ==='.'){
+            finalnum= finalnum.split("");
+            finalnum.splice(i,1); 
+           // console.log(finalnum);
+            if(finalnum[i] != null){
+                finalnum = finalnum.slice(0,i+2);
+           // console.log(finalnum)    
+            }
+         }//truncate extraneous digits
+    }
+  
+    //if (typeof finalnum != 'string'); 
+     
+     if( Object.prototype.toString.call(finalnum ) === '[object Array]' )
+        finalnum=finalnum.join(''); //if result is an array make it a string
+var tempmoney = [];    
+ for (var x=denominations.length-1; x>=0; x--) {
+ var denom = denominations[x];
+     var divided = Math.floor(finalnum / denom);
+     console.log(finalnum);
+     if (divided >= 1){
+        tempmoney[x] = divided;
+         //console.log(tempmoney);
+         finalnum -= divided * denominations[x];
+     }
+     if (finalnum ==0)
+         break;
+ 
+ }
+    return tempmoney;
 }
-var change = {
-  pennies : function (number){
-    num = currency(number);
-    num = parseInt(num.substring(num.length-3,num.length);
-    if(num < 25 && num % 25 < 5  )
-        return num
-    else if (num > 25 && num < 30)
-        return num % 25;
-    else if (num >29 && num <40 )
-        return (num % 25) - 10;
-    else if (
-  }
-  nickles : function (number){ 
-    num = currency(number);
-    num = parseInt(num.substring(num.length-3,num.length);
-    if(num >= 5 && num <= 9)
-        return 1;
-    else
-        return 0;
-}    
-  dimes: function (number){ 
-      num = currency(number);
-    num = parseInt(num.substring(num.length-3,num.length));
-    if(num < 25 && num >= 20)
-        return 2;
-    else if(num >=10 && num <=19 )
-        return 1;
-}
-  quarters: function (number){
-   num = currency(number);
-    num = parseInt(num.substring(num.length-3,num.length));
-    if(num % 25 ===0)
-        return num / 25;
-    else
-        return 0;}
-  ones: function (number){
-   num = currency(number);
-    num = parseInt(num[num.length-4]);
-    if(num % 5 !=0)
-        return num;
-    else
-        return 0;}
-  fives: function (number){
-   num = currency(number);
-    num = parseInt(num[num.length-4]);
-    if(num % 5===0)
-        return num / 5;
-    else
-        return 0;}
-  tens: function (number){
-   num = currency(number);
-    num = parseInt(num.substring(num.length-5,num.length-3));
-    if(num % 25 !=0)
-        return num;
-    else
-        return 0;}
-  twenties: function (number){
-   num = currency(number);
-    num = parseInt(num[num.length-1]);
-    if(num % 5 !=0)
-        return num;
-    else
-        return 0;}
-  fifties: function (number){
-   num = currency(number);
-    num = parseInt(num[num.length-1]);
-    if(num % 5 !=0)
-        return num;
-    else
-        return 0;}
-  hundreds: function (number){
-   num = currency(number);
-    num = parseInt(num[num.length-1]);
-    if(num % 5 !=0)
-        return num;
-    else
-        return 0;}
-    
-}
+
