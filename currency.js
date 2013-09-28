d5777209b24134a06072c5182aeed051c86b0e27
lafsar <nihlanth@gmail.com>
2013-09-28 02:04:01 -0600

    Done with currency
    Don't Know if result is formatted corectly

--- currency.js
+++ currency.js
@@ -12,7 +12,17 @@ For instance, given 7.23, it should return:
 
 //convert int, float or string to valid format first
 var currency = function (number) {
-var denominations = [1,5,10,25,100,500,1000,2000,5000,10000];  
+var denominations = [
+    {key: 'pennies: ', value:1},
+    {key:'nickels: ', value:5},
+    {key:'dimes: ', value:10},
+    {key:'quarters: ',value:25},
+    {key:'ones: ', value:100},
+    {key:'fives: ',value:500},
+    {key:'tens: ' ,value:1000},
+    {key:'twenties: ', value:2000},
+    {key:'fifties: ',value:5000},
+    {key:'hundreds: ',value:10000}];  
     var finalnum = number.toString();
     //console.log(finalnum);
     for(var i=0; i<=finalnum.length-1; i++) {
@@ -32,19 +42,23 @@ var denominations = [1,5,10,25,100,500,1000,2000,5000,10000];
      if( Object.prototype.toString.call(finalnum ) === '[object Array]' )
         finalnum=finalnum.join(''); //if result is an array make it a string
 var tempmoney = [];    
+    
  for (var x=denominations.length-1; x>=0; x--) {
  var denom = denominations[x];
-     var divided = Math.floor(finalnum / denom);
-     console.log(finalnum);
+     for(key in denom) {
+         if(denom.hasOwnProperty(key))
+             var val = denom[key];
+     }
+     
+     var divided = Math.floor(finalnum / val);
+     
      if (divided >= 1){
-        tempmoney[x] = divided;
-         //console.log(tempmoney);
-         finalnum -= divided * denominations[x];
+        document.writeln(denom.key,divided,',',"<br>")
+        console.log(denom.key,divided);
+        finalnum -= divided * val; 
      }
      if (finalnum ==0)
          break;
- 
  }
-    return tempmoney;
 }
 
